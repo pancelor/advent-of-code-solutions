5da2b8e72d81f86d8971187a259442e4a9fd0de6
pancelor <pancelor@gmail.com>
2018-12-02 01:21:07 -0700

    1 part 2; faster
    well, i assume it's faster... idk how to benchmark it atm

--- 1/main.go
+++ 1/main.go
@@ -22,24 +22,84 @@ func main() {
 	log.SetFlags(0)
 	log.SetPrefix("debug: ")
 
-	memory := make(map[int]bool)
+	reader := NewValReader(*infile)
+
+	seen := make(map[int]bool)
+	seen[0] = true
+	cumsums := make([]int, 0)
+
 	var total int
-	loop: for {
-		log.Println("again")
-		stream, err := os.Open(*infile)
-		check(err)
+	for val := range reader.Vals() {
+		total += val
+		if seen[total] {
+			fmt.Println(total)
+			return
+		}
+		seen[total] = true
+		cumsums = append(cumsums, total)
+	}
+	check(reader.Err())
+	// log.Println(cumsums)
+
+	for it := 1; true; it++ {
+		log.Println("it", it)
+		for _, val := range cumsums {
+			laterVal := val + it*total
+			// log.Printf("val %v, lval %v\n", val, laterVal)
+			if seen[laterVal] {
+				fmt.Println(laterVal)
+				return
+			}
+		}
+	}
+}
+
+type valReader struct {
+	filename string
+	c chan int
+	err error
+}
+
+func NewValReader(filename string) valReader {
+	c := make(chan int)
+	return valReader{
+		filename: filename,
+		c: c,
+	}
+}
+
+func (r *valReader) check(err error) bool {
+	if err != nil {
+		r.err = err
+		close(r.c)
+		return true
+	}
+	return false
+}
+
+func (r *valReader) Vals() chan int {
+	go func() {
+		stream, err := os.Open(r.filename)
+		if r.check(err) {
+			return
+		}
+
 		scanner := bufio.NewScanner(stream)
 		for scanner.Scan() {
 			val, err := strconv.Atoi(scanner.Text())
-			check(err)
-			seen := memory[total]
-			// log.Println(val, total, seen)
-			if seen {
-				fmt.Println(total)
-				break loop
+			if r.check(err) {
+				return
 			}
-			memory[total] = true
-			total += val
+			r.c <- val
 		}
-	}
+		if r.check(scanner.Err()) {
+			return
+		}
+		close(r.c)
+	}()
+	return r.c
+}
+
+func (r *valReader) Err() error {
+	return r.err
 }
