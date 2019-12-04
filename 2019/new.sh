253dc17595efe3ad4c6915b8464028bfc97c96e1
pancelor <pancelor@gmail.com>
2019-12-03 23:09:04 -0700

    oops forgot to remove that

--- 2019/new.sh
+++ 2019/new.sh
@@ -13,5 +13,3 @@ SUBLIME='/c/Program Files/Sublime Text 3/sublime_text.exe'
 CHROME='/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'
 "$SUBLIME" -n "$DAY" "$DAY"/main.go "$DAY"/input.txt
 "$CHROME" "${URL_PREFIX}/${DAY}/input" "${URL_PREFIX}/${DAY}"
-
-bash "$DAY"
