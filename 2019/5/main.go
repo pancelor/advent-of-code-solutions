c7cd1ce9b3930b04145ed9faf1eca572ebcda973
pancelor <pancelor@gmail.com>
2019-12-05 11:04:12 -0700

    2019.5.1 wip just need to use modes

--- 2019/5/main.go
+++ 2019/5/main.go
@@ -10,23 +10,48 @@ import (
 )
 
 func solve(memTemplate []int) (interface{}, error) {
-	targetRes := 19690720
-	for noun := 0; noun < len(memTemplate); noun++ {
-		for verb := 0; verb < len(memTemplate); verb++ {
-			mem := dupmem(memTemplate)
-			mem[1] = noun
-			mem[2] = verb
+	mem := dupmem(memTemplate)
+	inputs := []int{1}
+	res, err := run(mem, inputs)
+	if err != nil {
+		return nil, err
+	}
 
-			res, err := run(mem)
-			if err != nil {
-				continue
-			}
-			if res == targetRes {
-				return 100*noun + verb, nil
-			}
+	return res, nil
+}
+
+// Modes represent argument parameter modes
+// get(0) == true means the first parameter should be interpreted in
+//   immediate mode; false means position mode
+type Modes struct {
+	modes []bool
+}
+
+func (m *Modes) get(n int) bool {
+	assert(n >= 0, "bad get")
+	if n >= len(m.modes) {
+		return false
+	}
+	return m.modes[n]
+}
+
+func parseOpcode(code int) (int, Modes, error) {
+	s := strconv.Itoa(code)
+	opcode := code % 100
+	var modes []bool
+	for i := len(s) - 3; i >= 0; i-- {
+		// fmt.Printf("i=%v\n", i)
+		// fmt.Printf("s[i]=%v, '0'=%v, ==?: %v\n", s[i], '0', s[i] == '0')
+		if s[i] == '1' {
+			modes = append(modes, true)
+		} else if s[i] == '0' {
+			modes = append(modes, false)
+		} else {
+			return 0, Modes{}, fmt.Errorf("Unrecognized mode %q[%d]='%c'", s, i, s[i])
 		}
 	}
-	return nil, fmt.Errorf("No combination found")
+	// fmt.Printf("done\n")
+	return opcode, Modes{modes}, nil
 }
 
 func run(mem []int, inputs []int) ([]int, error) {
@@ -36,13 +61,22 @@ func run(mem []int, inputs []int) ([]int, error) {
 	pc := -1
 	var halt bool
 	for !halt {
-		opcode := chomp(mem, &pc)
+		code := chomp(mem, &pc)
+		opcode, modes, err := parseOpcode(code)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO
+		_ = modes
+
 		// dump(mem, pc)
 		switch opcode {
 		case 1: // add
 			a := chomp(mem, &pc)
 			b := chomp(mem, &pc)
 			c := chomp(mem, &pc)
+			// a = applyMode(modes.get(0))
 			ensureInbounds(mem, a, b, c)
 			av := mem[a]
 			bv := mem[b]
@@ -63,7 +97,7 @@ func run(mem []int, inputs []int) ([]int, error) {
 		case 4: // output
 			a := chomp(mem, &pc)
 			ensureInbounds(mem, a)
-			res = res.append(res, mem[a])
+			res = append(res, mem[a])
 		case 99: // halt
 			halt = true
 		default:
@@ -103,7 +137,27 @@ func dump(mem []int, pc int) {
 	fmt.Printf("\n]\n")
 }
 
+func test() {
+	opcode, modes, err := parseOpcode(1002)
+	assert(err == nil, "t1 err")
+	assert(opcode == 2, "t1 opcode")
+	assert(modes.get(0) == false, "t1 modes[0]")
+	assert(modes.get(1) == true, "t1 modes[1]")
+	assert(modes.get(2) == false, "t1 modes[2]")
+	assert(modes.get(100) == false, "t1 modes[100]")
+
+	opcode, modes, err = parseOpcode(3002)
+	assert(err != nil, "t2 err")
+
+	opcode, modes, err = parseOpcode(42)
+	assert(err == nil, "t3 err")
+
+	assert(false, "failfast")
+}
+
 func main() {
+	test()
+
 	input, err := getInput()
 	if err != nil {
 		panic(err)
