59ee2410cce8028759d7602a8bdca7f2cc3ff083
pancelor <pancelor@gmail.com>
2019-12-16 01:55:12 -0700

    cleanup

--- 2019/16/main.py
+++ 2019/16/main.py
@@ -1,33 +1,45 @@
 import itertools as itt
 
-def coeffs(n):
-  res = itt.repeat(0, n)
-  res = itt.chain(res, itt.repeat(1, n))
-  res = itt.chain(res, itt.repeat(0, n))
-  res = itt.chain(res, itt.repeat(-1, n))
-  res = itt.cycle(res)
-  res.next()
-  return res
+#
+# PART 1
+#
 
-def step(vals):
-  for i in range(len(vals)):
-    k = 0
-    for a, b in zip(vals, coeffs(i+1)):
-      k += a*b
-    yield abs(k) % 10
+# def coeffs(n):
+#   res = itt.repeat(0, n)
+#   res = itt.chain(res, itt.repeat(1, n))
+#   res = itt.chain(res, itt.repeat(0, n))
+#   res = itt.chain(res, itt.repeat(-1, n))
+#   res = itt.cycle(res)
+#   res.next()
+#   return res
 
-def stepMany(vals, n):
-  for i in range(n):
-    # print "step", i
-    vals = list(step(vals))
-  return vals
+# def step(vals):
+#   for i in range(len(vals)):
+#     k = 0
+#     for a, b in zip(vals, coeffs(i+1)):
+#       k += a*b
+#     yield abs(k) % 10
 
-def listToDec(vals):
-  total = 0
-  for x in vals:
-    total *= 10
-    total += x
-  return total
+# def stepMany(vals, n):
+#   for i in range(n):
+#     # print "step", i
+#     vals = list(step(vals))
+#   return vals
+
+# def listToDec(vals):
+#   total = 0
+#   for x in vals:
+#     total *= 10
+#     total += x
+#   return total
+
+# vals = map(int, raw_input())
+# res = stepMany(vals, 100)
+# print listToDec(res[:8])
+
+#
+# PART 2
+#
 
 import operator as op
 from functools import reduce
@@ -41,6 +53,7 @@ def ncr(n, r):
 def MPowerTopRowIndex(p, i):
   # returns the ith member (0-based) of the top row of M^p,
   # where M is an upper triangular all ones matrix
+  # (each digit of the final answer involves a dot product between M^100 and vals)
   return ncr(p-1+i, p-1)
 
 
@@ -49,7 +62,6 @@ vals = map(int, raw_input())
 offset = listToDec(vals[:7])
 print "offset\n", offset
 
-# first digit
 final = 0
 for digit in range(8):
   total = 0
@@ -66,7 +78,3 @@ for digit in range(8):
   final *= 10
   final += total
 print final
-
-# res = stepMany(vals, 100)
-# print listToDec(res[:8])
-# print res
