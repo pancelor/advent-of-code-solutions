bcc446fdb39a4c1cc84410d329d7fd69685dc354
pancelor <pancelor@gmail.com>
2019-12-18 20:39:26 -0700

    reorganize code / add comments / etc

--- 2019/18/main.go
+++ 2019/18/main.go
@@ -10,7 +10,9 @@ import (
 var assert = helpers.Assert
 var check = helpers.Check
 
-var HighestKeyID KeyID
+//
+// General Program Setup
+//
 
 // NRobots is the number of robots in the maze
 // Also, it's the number of start tiles (@)
@@ -19,6 +21,43 @@ const NRobots = 4
 // NKeys is the number of keys in the maze.
 // It includes NRobots pseudo-keys that makes the precomputeKeyDistances() function easier to write
 const NKeys = 26 + NRobots
+
+func main() {
+	fmt.Printf("answer:\n%v\n", solve(getInput()))
+}
+
+func getInput() *Maze {
+	lines, err := helpers.GetLines()
+	check(err)
+
+	var numStartsSeen int
+	var maze Maze
+	for r, l := range lines {
+		if l == "" {
+			continue
+		}
+		var row []Tile
+		for c, b := range []byte(l) {
+			tile := parseTile(b)
+			row = append(row, tile)
+			p := point{x: c, y: r}
+			if b == '@' {
+				assert(numStartsSeen < NRobots, "maze has %d starts (so far) but code only works on %d starts", numStartsSeen, NRobots)
+				maze.starts[numStartsSeen] = p
+				numStartsSeen++
+			}
+		}
+
+		maze.tiles = append(maze.tiles, row)
+	}
+
+	assert(NRobots == numStartsSeen, "maze has %d starts but code only works on %d starts", numStartsSeen, NRobots)
+	return &maze
+}
+
+//
+// Basic Types
+//
 
 type TileType int
 
@@ -35,6 +74,11 @@ type Tile struct {
 	kid KeyID // optional; exists iff tag == TT_KEY or TT_LOCK
 }
 
+// HighestKeyID is the highest non-pseudo key yet assigned.
+// It's used to determine when a SolveState is .done()
+var HighestKeyID KeyID
+
+// NextStartID is used to assign the different starts different KeyIDs
 var NextStartID = 26
 
 func parseTile(b byte) Tile {
@@ -49,7 +93,11 @@ func parseTile(b byte) Tile {
 			NextStartID++
 			return t
 		} else if 'a' <= b && b <= 'z' {
-			return Tile{tag: TT_KEY, kid: KeyID(b - 'a')}
+			kid := KeyID(b - 'a')
+			if kid < 26 && kid > HighestKeyID {
+				HighestKeyID = kid
+			}
+			return Tile{tag: TT_KEY, kid: kid}
 		} else {
 			assert('A' <= b && b <= 'Z', "unknown char %s", b)
 			return Tile{tag: TT_LOCK, kid: KeyID(b - 'A')}
@@ -104,16 +152,29 @@ func (p point) neighbors() (res []point) {
 	return
 }
 
-type Maze struct {
-	tiles   [][]Tile
-	keyLocs map[KeyID]point // a cache
-	starts  [NRobots]point
+// KeyID is 0-25 for an alphabetic key, or 26-29 for the start pseudo-keys
+type KeyID int
+
+func (kid KeyID) String() string {
+	switch kid {
+	case 26:
+		return "@"
+	case 27:
+		return "$"
+	case 28:
+		return "%"
+	case 29:
+		return "&"
+	default:
+		return string(kid + 'a')
+	}
 }
 
-func makeMaze() *Maze {
-	return &Maze{
-		keyLocs: make(map[KeyID]point),
-	}
+// Maze represents the static, physical structure of the maze.
+// No dynamic solution state is included in this type
+type Maze struct {
+	tiles  [][]Tile
+	starts [NRobots]point // start locations for the robots
 }
 
 func (maze Maze) String() string {
@@ -127,46 +188,50 @@ func (maze Maze) String() string {
 	return s.String()
 }
 
-type SolveState struct {
-	keys     [NKeys]bool    // one per alphabet letter + four starts
-	lastKeys [NRobots]KeyID // lastKey for each of the four robots
-}
-
-// rid is which robot collected the key
-func (state SolveState) collect(rid int, lastKey KeyID, otherNewKeys [NKeys]bool) SolveState {
-	// since this isn't a pointer method, state is already a copy we can edit at will
-	for kid, val := range otherNewKeys {
-		// if val && kid != lastKey && !newState.keys[kid] {
-		// 	fmt.Println("force-collecting", kid)
-		// }
-		if val {
-			state.keys[kid] = true
-		}
-	}
-
-	state.keys[lastKey] = true // (redundant atm)
-	state.lastKeys[rid] = lastKey
-
-	return state
-}
-
-func makeSolveState() SolveState {
-	state := SolveState{}
-	for rid := 0; rid < NRobots; rid++ {
-		// rid: robot id
-		state = state.collect(rid, KeyID(26+rid), [NKeys]bool{}) // collect psuedo-keys at starting locations
-	}
-	return state
-}
-
+// keyAt returns the KeyID of the key at the given position,
+// or panics if no such key exists
 func (maze *Maze) keyAt(p point) KeyID {
 	tile := maze.tiles[p.y][p.x]
 	assert(tile.tag == TT_KEY, "tile at %s isn't a key; it's %v", p, tile)
 	return tile.kid
 }
 
-func (sd StateDist) String() string {
-	return fmt.Sprintf("%d %s", sd.dist, sd.state)
+// keyLoc returns the location of the given KeyID,
+// or panics if no such key exists
+func (maze *Maze) keyLoc(kid KeyID) point {
+	for r, row := range maze.tiles {
+		for c, tile := range row {
+			if tile.tag == TT_KEY && tile.kid == kid {
+				return point{x: c, y: r}
+			}
+		}
+	}
+	assert(false, "KeyID %s is noplace", kid)
+	return point{}
+}
+
+//
+// Complicated Types
+//
+
+// SolveState is a pointer-free value representing the state of an in-progress solution
+// This value must have no pointers (e.g. slices) so that it can be used as a hashmap key
+// fields:
+//   keys: the keys that the solution has collected
+//   lastKeys: the last key that each robot collected last; i.e. where each robot is
+// Note that this struct does _not_ include the distance that it took
+//   to reach this state; if you want that, see SolveStateDist
+type SolveState struct {
+	keys     [NKeys]bool    // one per alphabet letter + four starts
+	lastKeys [NRobots]KeyID // lastKey for each of the four robots
+}
+
+func makeSolveState() SolveState {
+	state := SolveState{}
+	for rid := 0; rid < NRobots; rid++ {
+		state = state.collect(rid, KeyID(26+rid), [NKeys]bool{}) // collect psuedo-keys at starting locations
+	}
+	return state
 }
 
 func (state SolveState) String() string {
@@ -181,28 +246,98 @@ func (state SolveState) String() string {
 	return s.String()
 }
 
-type StateDist struct {
+// collect collects the given key (and any in-the-way keys) and returns the new SolveState
+// fields:
+//   rid: which robot collected the key
+//   lastKey: which key to move to
+//   otherNewKeys: keys that the robot picked up along the way to lastKey
+// since this isn't a pointer method, state is already a copy we can edit at will
+func (state SolveState) collect(rid int, lastKey KeyID, otherNewKeys [NKeys]bool) SolveState {
+	for kid, val := range otherNewKeys {
+		// if val && kid != lastKey && !newState.keys[kid] {
+		// 	fmt.Println("force-collecting roadside key", kid)
+		// }
+		if val {
+			state.keys[kid] = true
+		}
+	}
+
+	state.keys[lastKey] = true // (redundant atm, but better safe)
+	state.lastKeys[rid] = lastKey
+
+	return state
+}
+
+// done returns whether the state is fully solved (i.e. all keys are collected)
+func (state *SolveState) done() bool {
+	for kid, haveKey := range state.keys {
+		if KeyID(kid) > HighestKeyID {
+			break
+		}
+		if !haveKey {
+			return false
+		}
+	}
+	return true
+}
+
+// hasAllKeys returns whether all keys in the given list have been collected
+func (state *SolveState) hasAllKeys(kids [NKeys]bool) bool {
+	// unset keys we're wondering about
+	// (kids is copied by value so we're not doing anything bad by editing it directly)
+	for kid, haveKey := range state.keys {
+		if haveKey {
+			kids[KeyID(kid)] = false
+		}
+	}
+
+	// are there any keys still set?
+	for _, v := range kids {
+		if v {
+			return false
+		}
+	}
+	return true
+}
+
+// SolveStateDist is a tuple storing how many steps it took to reach a certain SolveState
+// The distance is not a property of the SolveState itself; there may be different ways to
+// reach the same SolveState that took different numbers of steps
+type SolveStateDist struct {
 	state SolveState
 	dist  int
 }
 
-func solve(maze *Maze) interface{} {
-	// fmt.Printf("maze:\n%s\n", maze)
+func (sd SolveStateDist) String() string {
+	return fmt.Sprintf("%d %s", sd.dist, sd.state)
+}
 
-	keyDists := precomputeKeyDistances(maze)
+// solve returns the minimum number of steps it takes to solve the maze
+func solve(maze *Maze) int {
+	// fmt.Printf("maze:\n%s\n", maze)
+	return solveKD(precomputeKeyDistances(maze))
+}
+
+func solveKD(keyDists KeyDistances) int {
 	// fmt.Printf("keyDists:\n%s\n", keyDists)
 
-	var stateQueue []StateDist
-	stateQueue = append(stateQueue, StateDist{
+	// stateQueue holds the queue of partial solutions
+	// that we've tried / want to try
+	// (TODO: don't store the entire queue in memory; we only pop)
+	var stateQueue []SolveStateDist
+	stateQueue = append(stateQueue, SolveStateDist{
 		state: makeSolveState(),
 		dist:  0,
 	})
+
+	// bestDists stores the best distance for each SolveState seen so far
 	bestDists := make(map[SolveState]int)
+
 	var skipCount int
 	for i := 0; i < len(stateQueue); i++ {
 		current := stateQueue[i]
 		shouldPrint := i%10000 == 0
-		// shouldPrint = false
+		shouldPrint = false
 		if shouldPrint {
 			fmt.Printf("Cycle %d/%d:\n", i+1, len(stateQueue))
 			// fmt.Printf("  skipped:     %d\n", skipCount)
@@ -223,10 +358,10 @@ func solve(maze *Maze) interface{} {
 			}
 		}
 
-		for _, ktemp := range keyDists.availableKeys(&current.state) {
-			new := StateDist{
-				state: current.state.collect(ktemp.rid, ktemp.KeyID, ktemp.KeyDist.keys),
-				dist:  current.dist + ktemp.KeyDist.dist,
+		for _, ak := range current.state.availableKeys(keyDists) {
+			new := SolveStateDist{
+				state: current.state.collect(ak.rid, ak.KeyID, ak.KeyDist.keys),
+				dist:  current.dist + ak.KeyDist.dist,
 			}
 			if shouldPrint {
 				fmt.Printf("    ->: %s\n", new)
@@ -248,74 +383,34 @@ func solve(maze *Maze) interface{} {
 	return best
 }
 
-type KeyTemp struct {
+// AvailableKey is a tuple saying what key is available to what robot,
+// and what the KeyDist is
+type AvailableKey struct {
 	KeyDist
 	KeyID
 	rid int
 }
 
 // availableKeys returns keys that are available to be gotten
-// note: this will often return keys we've already gotten (useful as checkpoints)
-func (kd KeyDistances) availableKeys(state *SolveState) (res []KeyTemp) {
+// note: this will often return keys we've already gotten;
+// this is useful so we can consolidate duplicate states sooner
+func (state *SolveState) availableKeys(kd KeyDistances) (res []AvailableKey) {
 	for rid := 0; rid < NRobots; rid++ {
 		submap := kd[state.lastKeys[rid]]
 		for kid := KeyID(0); kid < NKeys; kid++ {
 			kdist, prs := submap[kid]
 			// fmt.Printf("  KeyDist(%s)=%s\n", kid, kdist)
 			if prs && state.hasAllKeys(kdist.locks) {
-				res = append(res, KeyTemp{KeyDist: kdist, KeyID: kid, rid: rid})
+				res = append(res, AvailableKey{KeyDist: kdist, KeyID: kid, rid: rid})
 			}
 		}
 	}
 	return
 }
 
-func (state *SolveState) done() bool {
-	for kid, haveKey := range state.keys {
-		if KeyID(kid) > HighestKeyID {
-			break
-		}
-		if !haveKey {
-			return false
-		}
-	}
-	return true
-}
-
-func (state *SolveState) hasAllKeys(kids [NKeys]bool) bool {
-	// unset keys we have
-	for kid, haveKey := range state.keys {
-		if haveKey {
-			kids[KeyID(kid)] = false
-		}
-	}
-
-	// are there any keys still set?
-	for _, v := range kids {
-		if v {
-			return false
-		}
-	}
-	return true
-}
-
-// KeyID is 0-25 for an alphabetic key, or 26-29 for the start pseudo-keys
-type KeyID int
-
-func (kid KeyID) String() string {
-	switch kid {
-	case 26:
-		return "@"
-	case 27:
-		return "$"
-	case 28:
-		return "%"
-	case 29:
-		return "&"
-	default:
-		return string(kid + 'a')
-	}
-}
+//
+// KeyDist computation
+//
 
 // KeyDistances keeps track of distances between keys.
 type KeyDistances map[KeyID]map[KeyID]KeyDist
@@ -332,6 +427,12 @@ func (kds KeyDistances) String() string {
 	return s.String()
 }
 
+// KeyDist represents the distance between two keys,
+// along with the locks that are blocking this path
+// and the keys that are "blocking" the path
+// (storing the keys helps us reduce the branching factor of our algorithm
+// by eagerly picking up keys that are along the path)
+// !!! This assumes the maze has no cycles - the whole algorithm falls apart otherwise!!!
 type KeyDist struct {
 	dist  int
 	locks [NKeys]bool
@@ -362,7 +463,7 @@ func (kd KeyDist) String() string {
 	return s.String()
 }
 
-// assumes the maze has no cycles - whole algorithm falls apart otherwise
+// assumes the maze has no cycles - the whole algorithm falls apart otherwise
 func precomputeKeyDistances(maze *Maze) KeyDistances {
 	res := make(map[KeyID]map[KeyID]KeyDist)
 	for kid := KeyID(0); kid < NKeys; kid++ {
@@ -405,7 +506,7 @@ func precomputeKeyDistancesFrom(maze *Maze, kid KeyID) map[KeyID]KeyDist {
 	res := make(map[KeyID]KeyDist)
 
 	visited := makeVisitTracker()
-	visited[maze.keyLocs[kid]] = VisitInfo{tag: VT_FRONTEIR}
+	visited[maze.keyLoc(kid)] = VisitInfo{tag: VT_FRONTEIR}
 	for {
 		p, info, ok := visited.nextToVisit()
 		if !ok {
@@ -447,48 +548,4 @@ func precomputeKeyDistancesFrom(maze *Maze, kid KeyID) map[KeyID]KeyDist {
 	}
 
 	return res
-}
-
-func main() {
-	input, err := getInput()
-	check(err)
-	answer := solve(input)
-	fmt.Printf("answer:\n%v\n", answer)
-}
-
-func getInput() (*Maze, error) {
-	lines, err := helpers.GetLines()
-	if err != nil {
-		return nil, err
-	}
-
-	var numStartsSeen int
-	maze := makeMaze()
-	for r, l := range lines {
-		if l == "" {
-			continue
-		}
-		var row []Tile
-		for c, b := range []byte(l) {
-			tile := parseTile(b)
-			row = append(row, tile)
-			p := point{x: c, y: r}
-			if b == '@' {
-				assert(numStartsSeen < NRobots, "maze has %d starts (so far) but code only works on %d starts", numStartsSeen, NRobots)
-				maze.starts[numStartsSeen] = p
-				numStartsSeen++
-			}
-			if tile.tag == TT_KEY {
-				maze.keyLocs[tile.kid] = p
-				if tile.kid < 26 && tile.kid > HighestKeyID {
-					HighestKeyID = tile.kid
-				}
-			}
-		}
-
-		maze.tiles = append(maze.tiles, row)
-	}
-
-	assert(NRobots == numStartsSeen, "maze has %d starts but code only works on %d starts", numStartsSeen, NRobots)
-	return maze, nil
 }
