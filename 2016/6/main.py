5fafbc4c07605d6dd23cdbe3c3ec375f27241e66
pancelor <pancelor@gmail.com>
2020-12-01 11:29:43 -0700

    2016.6

--- 2016/6/main.py
+++ 2016/6/main.py
@@ -26,24 +26,13 @@ def nonedict(d):
 def clamp(x,a,b):
 	return max(a,min(x,b))
 
-def walk(cmds):
-	facing=1
-	x=0
-	y=0
-	for t in cmds:
-		if t=="\n":
-			yield x,y
-		else:
-			dx,dy={
-				"U": (0,-1),
-				"D": (0,1),
-				"L": (-1,0),
-				"R": (1,0),
-			}[t]
-			# print t,dx,dy
-			x=clamp(x+dx,-1,1)
-			y=clamp(y+dy,-1,1)
-
-tokens=sys.stdin.read()
-for x,y in walk(tokens):
-	print (y+1)*3+(x+1)+1,
+counts=[Counter(),Counter(),Counter(),Counter(),Counter(),Counter(),Counter(),Counter(),]
+for line in sys.stdin:
+	for i,ch in enumerate(line.strip()):
+		assert(i<8)
+		counts[i][ch]+=1
+r=[]
+for i in range(8):
+	x=counts[i].most_common()[-1:][0][0]
+	r.append(x)
+print ''.join(r)
